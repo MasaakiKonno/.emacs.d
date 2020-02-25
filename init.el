f3e9ecc065e6f6196aec77e938cd2270082eceb0
MasaakiKonno <konno@lifematics.co.jp>
2020-02-25 13:48:41 +0900

    Initial commit. Add submodule 'MasaakiKonno/cyberpunk-theme.el'.

---
+++ init.el
@@ -0,0 +1,28 @@
+;; Change the key of deleting to 'ctrl+h'
+(global-set-key "\C-h" 'delete-backward-char)
+
+;; Enable auto-fill mode
+(turn-on-auto-fill)
+
+;; Number of characters when turning up line
+(setq fill-column 80)
+
+;; Require auto-save-buffers package
+(require 'auto-save-buffers)
+(run-with-idle-timer 1.0 t 'auto-save-buffers) 
+
+;; Truncate long lines
+(set-default 'truncate-lines t)
+
+;;; Load path and load theme 'cyberpunk'
+(add-to-list 'custom-theme-load-path' "~/.emacs.d/cyberpunk-theme.el")
+(load-theme 'cyberpunk t)
+
+(setq make-backup-files nil)
+(setq auto-save-default nil)
+
+;; Show key strokes in minibuffer quickly.
+(setq echo-keystrokes 0.1)
+
+;; Use UTF-8
+(prefer-coding-system 'utf-8)
